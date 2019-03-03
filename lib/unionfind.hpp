cf7d226c80178d67efc889e6d24db29ff6c76d73
ritosonn <ritosonn@gmail.com>
2019-03-03 23:41:51 +0900

    連結成分の個数を前処理で求めるように変更

--- lib/unionfind.hpp
+++ lib/unionfind.hpp
@@ -1,6 +1,6 @@
 class UnionFind{
 private:
-    int length;
+    int length, numComponent;
     //parent[x]=xの親の番号 ............. xに親が存在する場合,
     //parent[x]=-(xを含む集合の要素数) ... xが親である場合
     int *parent;
@@ -12,10 +12,11 @@ public:
     void unite(int x,int y);
     bool isSameTree(int x,int y);
     int sizeOf(int x);
-    int count();
+    int component();
 };
 UnionFind::UnionFind(int l){
     length=l;
+    numComponent=l;
     parent=new int[l];
     for(int i=0;i<l;i++)parent[i]=-1;
     rank=new int[l];
@@ -31,30 +32,24 @@ int UnionFind::root(int x){
 }
 void UnionFind::unite(int x,int y){
     int rx=root(x),ry=root(y);
-    if(rank[rx]>rank[ry]){
-        parent[rx]+=parent[ry];
-        parent[ry]=rx;
-    }
-    else if(rank[rx]<rank[ry]){
-        parent[ry]+=parent[rx];
-        parent[rx]=ry;
-    }
-    else if(rx!=ry){
-        parent[rx]+=parent[ry];
-        parent[ry]=rx;
-        rank[rx]+=1;
+    if(rx!=ry){
+        numComponent--;
+        if(rank[rx]>rank[ry]){
+            parent[rx]+=parent[ry];
+            parent[ry]=rx;
+        }
+        else{
+            if(rank[rx]==rank[ry])rank[ry]++;
+            parent[ry]+=parent[rx];
+            parent[rx]=ry;
+        }
     }
 }
 bool UnionFind::isSameTree(int x,int y){
     return root(x)==root(y);
 }
-int UnionFind::count(){
-    int *tmp=new int[length];
-    for(int i=0;i<length;i++)tmp[i]=0;
-    for(int i=0;i<length;i++)tmp[root(i)]=1;
-    int ans=0;
-    for(int i=0;i<length;i++)ans+=tmp[i];
-    return ans;
+int UnionFind::component(){
+    return numComponent;
 }
 int UnionFind::sizeOf(int x){
     return -parent[root(x)];
