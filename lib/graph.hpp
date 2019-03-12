dbeb0aa6590c0300295c50bbb57488365537cce3
ritosonn <ritosonn@gmail.com>
2019-03-13 03:14:18 +0900

    最大流（Ford-Fulkerson法）を実装

--- lib/graph.hpp
+++ lib/graph.hpp
@@ -1,148 +1,144 @@
 #include<cstdio>
 #include<vector>
 #include<queue>
-#include<functional>
-typedef long long weight;
-weight const infty = 0x3fffffffffffff;
-typedef std::pair<weight,int> edge;
+#include<algorithm>
+#include<unordered_set>
+long long const infty = 0x3fffffffffffff;
+struct edge{
+    long long weight;
+    int to,rev;
+};
+//隣接リスト表現
 class Graph{
-private:
+protected:
     int numVertex,numEdge;
     std::vector<edge> *adj;
 public:
-    Graph(int v);
-    ~Graph();
-    void addEdge(int u,int v,weight w=1LL,bool isDirected=false);
-    int getNumVertex();
-    int getNumEdge();
-    void dijkstra(weight l[],int s,int prev[]=NULL);
-    void debugPrint();
-    void connectedComponent(bool flag[],int s);
-    std::pair<weight,int> farthestPoint(int s);
-    weight diameter();
-    bool isConnected();
-    bool isTree();
-};
+    //---基本---
 
-//Graph g(v) ... make a new 0-indexed graph with v vertex.
-Graph::Graph(int v){
-    numVertex=v;
-    numEdge=0;
-    adj=new std::vector<edge>[v];
-}
-
-//destructor
-Graph::~Graph(){
-    delete[] adj;
-}
-
-int Graph::getNumEdge(){
-    return numEdge;
-}
-
-int Graph::getNumVertex(){
-    return numVertex;
-}
-
-// add u->v (or u-v) edge with weight w
-void Graph::addEdge(int u,int v,weight w,bool isDirected){
-    adj[u].push_back({w,v});
-    numEdge++;
-    if(!isDirected){
-        adj[v].push_back({w,u});
+    //v頂点のグラフを作成する（頂点の番号は0-indexed）
+    Graph(int v){
+        numVertex=v;
+        numEdge=0;
+        adj=new std::vector<edge>[v];
+    }
+    ~Graph(){ delete[] adj; }
+    int getNumVertex(){ return numVertex; }
+    int getNumEdge(){ return numEdge; }
+    void addEdge(int u,int v,long long w=1LL){
+        adj[u].push_back({w,v,-1});
         numEdge++;
     }
-}
+    //フロー計算用の有向辺+逆辺の追加
+    void addNetwork(int u,int v,long long w=1LL){
+        adj[u].push_back({w,v,(int)adj[v].size()});
+        adj[v].push_back({0,u,(int)adj[u].size()-1});
+        numEdge++;
+    }
 
-// shortest path length, O(E+VlogV)
-void Graph::dijkstra(weight l[],int s,int prev[]){
-    for(int i=0;i<numVertex;i++)l[i]=infty;
-    if(prev!=NULL)for(int i=0;i<numVertex;i++)prev[i]=-1;
-    l[s]=0;
-    std::priority_queue<edge,std::vector<edge>,std::greater<edge>> pq;
-    pq.push({0,s});
-    while(!pq.empty()){
-        int u=pq.top().second;
-        pq.pop();
-        for(auto i=adj[u].begin();i!=adj[u].end();i++){
-            int v=(*i).second;
-            weight w=(*i).first;
-            if(l[v]>l[u]+w){
-                l[v]=l[u]+w;
-                if(prev!=NULL)prev[v]=u;
-                pq.push({l[v],v});
+    //---一般のグラフに使えるアルゴリズム---
+
+    //ダイクストラ法（単一頂点を始点とする最短経路）O(E+VlogV)
+    //制約 : weight>=0
+    void shortestPath_Dijkstra(long long l[],int s,int prev[]=NULL){
+        for(int i=0;i<numVertex;i++)l[i]=infty;
+        if(prev!=NULL)for(int i=0;i<numVertex;i++)prev[i]=-1;
+        l[s]=0;
+        auto cmp=[](const edge &a,const edge &b)->bool { return a.weight>b.weight; };
+        std::priority_queue<edge,std::vector<edge>,decltype(cmp)> pq(cmp);
+        pq.push({0,s,-1});
+        while(!pq.empty()){
+            int u=pq.top().to;
+            pq.pop();
+            for(auto i=adj[u].begin();i!=adj[u].end();i++){
+                int v=(*i).to;
+                long long w=(*i).weight;
+                if(l[v]>l[u]+w){
+                    l[v]=l[u]+w;
+                    if(prev!=NULL)prev[v]=u;
+                    pq.push({l[v],v,-1});
+                }
             }
         }
     }
-}
 
-void Graph::connectedComponent(bool flag[],int s){
-    for(int i=0;i<numVertex;i++)flag[i]=false;
-    std::queue<int> q;
-    q.push(s);
-    while(!q.empty()){
-        int v=q.front();
-        q.pop();
-        flag[v]=true;
-        for(auto i=adj[v].begin();i!=adj[v].end();i++){
-            if(!flag[(*i).second]){
-                flag[(*i).second]=true;
-                q.push((*i).second);
+    //sと同じ連結成分にある頂点集合を返す
+    std::unordered_set<int> isSameComponent(int s){
+        std::queue<int> q;
+        std::unordered_set<int> ans;
+        q.push(s);
+        while(!q.empty()){
+            int v=q.front();
+            ans.emplace(v);
+            q.pop();
+            for(auto i=adj[v].begin();i!=adj[v].end();i++){
+                int w=(*i).to;
+                q.push(w);
             }
         }
+        return ans;
     }
-}
-
-bool Graph::isConnected(){
-    bool *flag=new bool[numVertex];
-    connectedComponent(flag,0);
-    for(int i=0;i<numVertex;i++){
-        if(!flag[i])return false;
+    bool isConnected(){
+        return isSameComponent(0).size()==numVertex;
     }
-    return true;
-}
-
-// farthest point of tree
-std::pair<weight,int> Graph::farthestPoint(int s){
-    std::queue<int> q;
-    weight *ws=new weight[numVertex];
-    for(int i=0;i<numVertex;i++)ws[i]=0;
-    q.push(s);
-    while(!q.empty()){
-        int v=q.front();
-        q.pop();
-        for(auto i=adj[v].begin();i!=adj[v].end();i++){
-            if(ws[(*i).second]==0){
-                ws[(*i).second]=ws[v]+(*i).first;
-                q.push((*i).second);
+    bool isTree(){
+        return isConnected()&&(numEdge+1==numVertex);
+    }
+    
+private:
+    //vからtへの増加パスをdfsで探す
+    //fは流量の上界のうち現時点で判明している最小のもの
+    long long dfs_Ford_fulkerson(int v,int t,long long f,bool* used){
+        //始点と終点が一致していれば無限に流せる
+        if(v==t)return f;
+        used[v]=true;
+        //vから出る各辺eに対して、
+        for(auto itr=adj[v].begin();itr!=adj[v].end();itr++){
+            edge e=*itr;
+            //e.toにまだ訪問したことがなく、さらに流せるとき
+            if(!used[e.to]&&e.weight>0){
+                //e.toから終点tまで流せる量を取得する
+                //流量の上界は、eで流せる量でも上から抑えられる
+                long long d = dfs_Ford_fulkerson(e.to, t, std::min(f,e.weight), used);
+                //もし流せるなら
+                if(d>0){
+                    //dだけ逆流させればvからtにもd流せる
+                    itr->weight-=d;
+                    adj[e.to][e.rev].weight+=d;
+                    return d;
+                }
             }
         }
-    };
-    int v=0;
-    weight w=ws[0]; 
-    for(int i=1;i<=numVertex;i++){
-        if(w<ws[i]){
-            v=i;
-            w=ws[i];
+        //どこも訪問済みか流せる量がないときは0
+        return 0;
+    }
+public:
+    //最大流 O(E*(最大流の流量))
+    long long maxFlow_Ford_Fulkerson(int s,int t){
+        bool *used=new bool[numVertex];
+        long long flow=0;
+        while(true){
+            for(int i=0;i<numVertex;i++)used[i]=false;
+            long long f=dfs_Ford_fulkerson(s,t,infty,used);
+            if(f==0)return flow;
+            flow+=f;
         }
     }
-    delete[] ws;
-    return {w,v};
-}
 
-weight Graph::diameter(){
-    std::pair<weight,int> p=farthestPoint(0),q=farthestPoint(p.second);
-    return q.first;
-}
+    //----debug----
 
-//------------ for debug ------------
-void Graph::debugPrint(){
-    printf("numVertex: %d\n",numVertex);
-    for(int i=0;i<numVertex;i++){
-        printf("%4d: ",i);
-        for(auto itr=adj[i].begin();itr!=adj[i].end();itr++){
-            printf("(%d->%d:%lld)",i,(*itr).second,(*itr).first);
+    void debugPrint(){
+        printf("numEdge: %d\n",numEdge);
+        printf("numVertex: %d\n",numVertex);
+        printf("isConnected: %s\n",isConnected()?"True":"False");
+        printf("isTree: %s\n",isTree()?"True":"False");
+        printf("EdgeList:\n");
+        for(int i=0;i<numVertex;i++){
+            printf("%6d: ",i);
+            for(auto itr=adj[i].begin();itr!=adj[i].end();itr++){
+                printf("(%d->%d:%lld)",i,(*itr).to,(*itr).weight);
+            }
+            printf("\n");
         }
-        printf("\n");
     }
-}
+};
